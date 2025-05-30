4585a63b6a668be286725fbc321bfbd814b62c08
Sefinek <contact@sefinek.net>
2025-05-30 02:41:07 +0200

    Use `require('express').Router()`

--- www/routes/Main.js
+++ www/routes/Main.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const UpdateDiscordRolesController = require('../controllers/UpdateDiscordRoles.js');
 const RemoveDiscordRolesController = require('../controllers/RemoveDiscordRoles.js');
